0e8ea7bbf82cc0f1141300089fa9a71f11d0b4af
Mohammed Safwan S <mdsafwan3102001@gmail.com>
2021-09-29 13:28:28 +0530

    EMOJIS!

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   
   return (
     <div className ="h-screen w-full flex flex-col justify-center items-center bg-green-300">
-      <h1 className = "p-5 text-center font-neon text-4xl">Weatherman ⛈️</h1>
+      <h1 className = "p-5 text-center font-neon text-4xl">Weatherman ⛈️ ☀️ ❄️</h1>
       <WeatherCard></WeatherCard>
     </div>
   )
